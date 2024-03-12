f0035e800e5cb0be40e6564166419b183870ca6d
Konjak2e8 <734977762@qq.com>
2024-03-12 20:49:06 +0800

    fix: modify makefile for running

---
+++ tgaimage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 73497 on 12/03/2024.
+//
+
+#include "tgaimage.h"
---
+++ tgaimage.h
@@ -0,0 +1,16 @@
+//
+// Created by 73497 on 12/03/2024.
+//
+
+#ifndef TINYRENDERER_TGAIMAGE_H
+#define TINYRENDERER_TGAIMAGE_H
+
+
+
+class tgaimage {
+
+};
+
+
+
+#endif //TINYRENDERER_TGAIMAGE_H
